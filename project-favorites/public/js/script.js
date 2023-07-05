f1ba568cce0d8f2a32c825bc9d6469bf23e5214a
kalebesilva <kalebe.silvadev@gmail.com>
2023-07-04 20:35:48 -0500

    Concertando Script para add na lista

--- project-favorites/public/js/script.js
+++ project-favorites/public/js/script.js
@@ -3,12 +3,14 @@ var form = document.querySelector(".form");
 var li = document.createElement("li");
 if (form) {
     form.addEventListener("submit", function (event) {
-        event.preventDefault;
+        event.preventDefault();
         if (form != null || form != undefined) {
             var myformOBJ = new FormData(form);
             var data = myformOBJ.get("input");
             var _a = data.split(","), name_1 = _a[0], url = _a[1];
             var obj = returnObj(name_1, url);
+            li.textContent = "".concat(obj.name, " ").concat(obj.url);
+            ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
         }
     });
 }
--- project-favorites/public/js/script.ts
+++ project-favorites/public/js/script.ts
@@ -2,33 +2,36 @@ let ul: Element | null = document.querySelector(".lista");
 let form: HTMLFormElement | null = document.querySelector(".form");
 let li: HTMLElement = document.createElement("li");
 
+interface myObjForAddInList {
+  name: string,
+  url: string
+}
 
 
 
 if (form) {
   form.addEventListener("submit", (event: SubmitEvent) => {
-    event.preventDefault;
+    
+    event.preventDefault();
     
     if (form != null || form != undefined) {
 
       let myformOBJ: FormData = new FormData(form);
       let data: string | null =  myformOBJ.get("input") as string;
       let [name, url]: string[] = data.split(",");
-      let obj: Object | null = returnObj(name, url);
-      li.textContent = `${obj}`;
+      let obj: myObjForAddInList | null = returnObj(name, url);
+      li.textContent = `${obj.name} ${obj.url}`;
       ul?.appendChild(li);
 
-      console.log(ul)
-
-      
+  
       
     }
   });
 }
 
 
-function returnObj(name: string, url: string): Object | null{
-  return {
+function returnObj(name: string, url: string): myObjForAddInList{
+  return  {
     name: name,
     url: url
   }
