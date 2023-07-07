b31cccf421caeef958b693156db4ee8881367365
kalebesilva <kalebe.silvadev@gmail.com>
2023-07-06 23:14:07 -0500

    Fix error for pass JSON in body request

--- project-favorites/backend/api/app.ts
+++ project-favorites/backend/api/app.ts
@@ -7,8 +7,10 @@ import myRouters from "./routers"
 
 const app = express();
 
+app.use(express.json())
+
 app.use(myRouters);
 
-app.listen(5000, ()=>{
+app.listen(3000, ()=>{
     console.log("API Running");
 })
