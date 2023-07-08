c5376cfb740a6053709651bde2852511b698e3a5
kalebesilva <kalebe.silvadev@gmail.com>
2023-07-07 23:36:27 -0500

    insert funcionando no front - correcao

--- project-favorites/frontend/public/js/script.js
+++ project-favorites/frontend/public/js/script.js
@@ -41,16 +41,26 @@ async function getUrlItemData(url) {
   return data;
 }
 
-async function insertNewUrlItem(url, obj) {
-  window.alert(obj)
+// Example POST method implementation:
+async function postData(url, data) {
+  // Default options are marked with *
   const response = await fetch(url, {
-    
-    method: "POST",
-    body: obj
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
-  const data = await response.json();
-  return data;
+  return response.json(); // parses JSON response into native JavaScript objects
 }
+
+
 
 
 if (form) {
@@ -60,7 +70,7 @@ if (form) {
     if (form != null || form != undefined) {
       let myformOBJ = new FormData(form);
       let data = myformOBJ.get("input");
-      insertNewUrlItem(urlApi, returnJsonFileByForm(data))
+      postData(urlApi, returnJsonFileByForm(data))
     }
   });
 }
